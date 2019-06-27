8019334bb2bad035df8504e83928ff51225d4bc4
Sonu Sourav <sonusouravdx001@gmail.com>
2019-06-27 12:44:05 +0530

    reverted

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 
+const hostname = '0.0.0.0';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
@@ -8,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, () => {
-  console.log(`Server running at ${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
